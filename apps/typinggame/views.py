177a6c280fc56d7a8a03ecfadbd5a8faa0be2d14
Anna Yanchuk <itsme03anna.yanchuk@ognivo.net>
2025-07-31 01:50:16 +0900

    Refactor questions data formatting for improved readability

--- apps/typinggame/views.py
+++ apps/typinggame/views.py
@@ -5,18 +5,18 @@ import json
 def index(request):
     # For simplicity, questions are hardcoded here.
     # In a real application, you would fetch these from a database.
-  questions_data = [
-    {'id': 1, 'japanese': 'エビ好きに悪人はいねえ', 'romaji': 'ebizukiniakuninhainee'},
-    {'id': 2, 'japanese': 'エンタープライズ発進', 'romaji': 'entapuraizuhasshin'},
-    {'id': 3, 'japanese': '俺がガンダムだ', 'romaji': 'oregagandamuda'},
-    {'id': 4, 'japanese': 'これがシュタインズゲートの選択だよ', 'romaji': 'koregasyutainzgeetonosentakudayo'},
-    {'id': 5, 'japanese': '知っているのか雷電', 'romaji': 'shitteirunokaraiden'},
-    {'id': 6, 'japanese': 'こいつ動くぞ', 'romaji': 'koitsuugokuzo'},
-    {'id': 7, 'japanese': 'ご照覧あれい', 'romaji': 'goshouranarei'},
-    {'id': 8, 'japanese': 'お前が本当の橋になれ', 'romaji': 'omaegahontounohashininare'},
-    {'id': 9, 'japanese': '誉れは浜で死にました', 'romaji': 'homorewahamadeshinimashita'},
-    {'id': 10, 'japanese': '私は見たぞ', 'romaji': 'watashiwomitazo'}
-]
+    questions_data = [
+        {'id': 1, 'japanese': 'エビ好きに悪人はいねえ', 'romaji': 'ebizukiniakuninhainee'},
+        {'id': 2, 'japanese': 'エンタープライズ発進', 'romaji': 'entapuraizuhasshin'},
+        {'id': 3, 'japanese': '俺がガンダムだ', 'romaji': 'oregagandamuda'},
+        {'id': 4, 'japanese': 'これがシュタインズゲートの選択だよ', 'romaji': 'koregasyutainzgeetonosentakudayo'},
+        {'id': 5, 'japanese': '知っているのか雷電', 'romaji': 'shitteirunokaraiden'},
+        {'id': 6, 'japanese': 'こいつ動くぞ', 'romaji': 'koitsuugokuzo'},
+        {'id': 7, 'japanese': 'ご照覧あれい', 'romaji': 'goshouranarei'},
+        {'id': 8, 'japanese': 'お前が本当の橋になれ', 'romaji': 'omaegahontounohashininare'},
+        {'id': 9, 'japanese': '誉れは浜で死にました', 'romaji': 'homorewahamadeshinimashita'},
+        {'id': 10, 'japanese': '私は見たぞ', 'romaji': 'watashiwomitazo'}
+    ]
     
     first_question = questions_data[0] if questions_data else None
 
