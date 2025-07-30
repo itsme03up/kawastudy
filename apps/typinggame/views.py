779efd0d5cf95653d5a8ef5a393af3caceb80f99
Anna Yanchuk <itsme03anna.yanchuk@ognivo.net>
2025-07-31 01:52:42 +0900

    Fix romaji formatting for consistency in questions data

--- apps/typinggame/views.py
+++ apps/typinggame/views.py
@@ -7,15 +7,15 @@ def index(request):
     # In a real application, you would fetch these from a database.
     questions_data = [
         {'id': 1, 'japanese': 'エビ好きに悪人はいねえ', 'romaji': 'ebizukiniakuninhainee'},
-        {'id': 2, 'japanese': 'エンタープライズ発進', 'romaji': 'entapuraizuhasshin'},
+        {'id': 2, 'japanese': 'エンタープライズ発進', 'romaji': 'enta-puraizuhasshin'},
         {'id': 3, 'japanese': '俺がガンダムだ', 'romaji': 'oregagandamuda'},
-        {'id': 4, 'japanese': 'これがシュタインズゲートの選択だよ', 'romaji': 'koregasyutainzgeetonosentakudayo'},
+        {'id': 4, 'japanese': 'これがシュタインズゲートの選択だよ', 'romaji': 'koregasyutainzuge-tonosentakudayo'},
         {'id': 5, 'japanese': '知っているのか雷電', 'romaji': 'shitteirunokaraiden'},
-        {'id': 6, 'japanese': 'こいつ動くぞ', 'romaji': 'koitsuugokuzo'},
-        {'id': 7, 'japanese': 'ご照覧あれい', 'romaji': 'goshouranarei'},
+        {'id': 6, 'japanese': 'こいつ動くぞ', 'romaji': 'koituugokuzo'},
+        {'id': 7, 'japanese': 'ご照覧あれい', 'romaji': 'gosyouranarei'},
         {'id': 8, 'japanese': 'お前が本当の橋になれ', 'romaji': 'omaegahontounohashininare'},
-        {'id': 9, 'japanese': '誉れは浜で死にました', 'romaji': 'homorewahamadeshinimashita'},
-        {'id': 10, 'japanese': '私は見たぞ', 'romaji': 'watashiwomitazo'}
+        {'id': 9, 'japanese': '誉れは浜で死にました', 'romaji': 'homarewahamadeshinimashita'},
+        {'id': 10, 'japanese': '私は見たぞ', 'romaji': 'watashiwamitazo'}
     ]
     
     first_question = questions_data[0] if questions_data else None
