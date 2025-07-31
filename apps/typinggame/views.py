23e7119dc5c7319e39bbb572bb01b450e2568250
Anna Yanchuk <itsme03anna.yanchuk@ognivo.net>
2025-07-31 12:11:53 +0900

    fix: Correct romaji spelling and comment out unused questions in index view

--- apps/typinggame/views.py
+++ apps/typinggame/views.py
@@ -7,20 +7,20 @@ def index(request):
     # In a real application, you would fetch these from a database.
     questions_data = [
         {'id': 1, 'japanese': 'エビ好きに悪人はいねえ', 'romaji': 'ebizukiniakuninhainee'},
-        {'id': 2, 'japanese': 'エンタープライズ発進', 'romaji': 'enta-puraizuhasshin'},
+        {'id': 2, 'japanese': 'エンタープライズ発進', 'romaji': 'enta-puraizuhassin'},
         {'id': 3, 'japanese': '俺がガンダムだ', 'romaji': 'oregagandamuda'},
-        {'id': 4, 'japanese': 'これがシュタインズゲートの選択だよ', 'romaji': 'koregasyu-tainzuge-tonosentakudayo'},
+        {'id': 4, 'japanese': 'これがシュタインズゲートの選択だよ', 'romaji': 'koregasyutainzuge-tonosentakudayo'},
         {'id': 5, 'japanese': '知っているのか雷電', 'romaji': 'shitteirunokaraiden'},
         {'id': 6, 'japanese': 'こいつ動くぞ', 'romaji': 'koituugokuzo'},
         {'id': 7, 'japanese': 'ご照覧あれい', 'romaji': 'gosyouranarei'},
         {'id': 8, 'japanese': 'お前が本当の橋になれ', 'romaji': 'omaegahontounohashininare'},
         {'id': 9, 'japanese': '誉れは浜で死にました', 'romaji': 'homarewahamadeshinimashita'},
         {'id': 10, 'japanese': '私は見たぞ', 'romaji': 'watashiwamitazo'},
-        {'id': 11, 'japanese': 'やっぱりNO.1!', 'romaji': 'yappari no.1!'},
-        {'id': 12, 'japanese': 'チャーハン作るよ!', 'romaji': 'cha-hantsukuruyo!'},
-        {'id': 13, 'japanese': 'コーヒーブレイク', 'romaji': 'ko-hi-bureiku'},
-        {'id': 14, 'japanese': 'メールアドレス', 'romaji': 'me-ruadoresu'},
-        {'id': 15, 'japanese': 'プログラミング', 'romaji': 'puroguramingu'},
+        # {'id': 11, 'japanese': 'やっぱりNO.1!', 'romaji': 'yappari no.1!'},
+        # {'id': 12, 'japanese': 'チャーハン作るよ!', 'romaji': 'cha-hantsukuruyo!'},
+        # {'id': 13, 'japanese': 'コーヒーブレイク', 'romaji': 'ko-hi-bureiku'},
+        # {'id': 14, 'japanese': 'メールアドレス', 'romaji': 'me-ruadoresu'},
+        # {'id': 15, 'japanese': 'プログラミング', 'romaji': 'puroguramingu'},
     ]
     
     first_question = questions_data[0] if questions_data else None
