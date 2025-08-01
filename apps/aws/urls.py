69eadd48505272fe306ebeca4ba60a88adfb2f52
Anna Yanchuk <itsme03anna.yanchuk@ognivo.net>
2025-08-01 23:05:14 +0900

    feat: Add detailed column list view with enhanced content for AWS benefits

--- apps/aws/urls.py
+++ apps/aws/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'aws'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('columns/', views.column_list, name='column_list'),
-    path('quizzes/', views.quiz_list, name='quiz_list'),
+    path('', views.index, name='index'),  # AWSトップページ
+    path('columns/', views.column_list, name='column_list'),  # コラム一覧
+    path('quizzes/', views.quiz_list, name='quiz_list'),      # クイズ一覧
 ]
